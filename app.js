b8843e899e5bdde3d65a6a79537371a89dbd1212
ananijoel <ananijoellanatide@gmail.com>
2024-07-01 06:52:54 +0000

    feat:game of data, linking an id with a name using the mock-pokemon.js file who contain a list of pokemons

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express') //recuperation du packet express
+let pokemons = require('./mock-pokemon')
 
 const app = new express() // creation d'une instance de l'application express, le petit serveur web sur lequel l'API rest va fonctionner
 const port = 3000 // definition du port sur lequel on va demarrer l'API Rest
 
 app.get('/',(req,res) => res.send('hello again, express !')) // definition du premier point de terminaison (coeur d'express)
 app.get('/api/pokemons/:id',(req,res)  => {
-    const id  = req.params.id
-    res.send(`Vous avez demandéle pokemon n° ${id}`)
+    const id  = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send(`Vous avez demandéle pokémon ${pokemon.name}.`)
 })
 
 app.listen(port,() => console.log(`Notre premiere application Node est demarée sur : http://localhost:${port}`)) //demarage de l'API Rest sur le port 300 et affichage d'un message de confirmation dans le terminal grace a listennm
