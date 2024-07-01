9501d045e7300913c6ffd2bd7ed0384b9ca1daec
ananijoel <ananijoellanatide@gmail.com>
2024-07-01 21:30:00 +0000

    feat: first use of a middleware

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ let pokemons = require('./mock-pokemon')
 const app = new express() // creation d'une instance de l'application express, le petit serveur web sur lequel l'API rest va fonctionner
 const port = 3000 // definition du port sur lequel on va demarrer l'API Rest
 
+
+app.use((req,res, next) =>{
+    console.log(`URL : ${req.url}`)
+    next()
+})
+
 app.get('/',(req,res) => res.send('hello again, express !')) // definition du premier point de terminaison (coeur d'express)
 app.get('/api/pokemons/:id',(req,res)  => {
     const id  = parseInt(req.params.id)
