4f075fcc93a64cf7c5282f88ee1f6487ca5244c7
ananijoel <ananijoellanatide@gmail.com>
2024-07-01 06:36:58 +0000

    feat:parameter id in the road /api/pokemons/:id

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = new express() // creation d'une instance de l'application express, l
 const port = 3000 // definition du port sur lequel on va demarrer l'API Rest
 
 app.get('/',(req,res) => res.send('hello again, express !')) // definition du premier point de terminaison (coeur d'express)
-app.get('/api/pokemons/1',(req,res)  => res.send('hello, bulbizarre !'))
+app.get('/api/pokemons/:id',(req,res)  => {
+    const id  = req.params.id
+    res.send(`Vous avez demandéle pokemon n° ${id}`)
+})
 
 app.listen(port,() => console.log(`Notre premiere application Node est demarée sur : http://localhost:${port}`)) //demarage de l'API Rest sur le port 300 et affichage d'un message de confirmation dans le terminal grace a listennm
