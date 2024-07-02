2b7782b552273cc449512f57ac7029f4287934ec
ananijoel <ananijoellanatide@gmail.com>
2024-07-02 06:00:21 +0000

    feat:endpoint for modifying a pokemon

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app
     .use(bodyParser.json())// middleware qui sert a parser toutes les entres de la web app du format string au format json
 
 app.get('/',(req,res) => res.send('hello again, express !')) // definition du premier point de terminaison (coeur d'express)
+
 app.get('/api/pokemons/:id',(req,res)  => {
     const id  = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
@@ -35,5 +36,14 @@ app.post('/api/pokemons',(req,res) =>{
     res.json(success(message,pokemonCreated))
 })//point de terminaison qui permet d'ajouter un nouveau pokemon a la liste des pokemons grace a la methode post
 
+app.put('/api/pokemons/:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+    const pokemonUpdated = { ...req.body, id: id}
+    pokemons = pokemons.map(pokemon =>{
+        return pokemon.id === id ? pokemonUpdated : pokemon
+    })
+    const message = `Le pokemon ${pokemonUpdated.name} a bien été mis a modifié.`
+    res.json(success(message,pokemonUpdated))
+})
 
 app.listen(port,() => console.log(`Notre premiere application Node est demarée sur : http://localhost:${port}`)) //demarage de l'API Rest sur le port 300 et affichage d'un message de confirmation dans le terminal grace a listennm
