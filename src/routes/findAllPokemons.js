9e3b810cf125d1ebc6ea5e32087161d012fd3fb9
ananijoel <ananijoellanatide@gmail.com>
2024-07-06 06:52:08 +0000

    feat:limitaton de la proposition  de reponse a >2 characteres

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -5,6 +5,11 @@ module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
     if(req.query.name){
       const name = req.query.name
+      const limit = parseInt(req.query.limit) || 5
+      if(name.length < 2){
+        const message = `le terme de recherche dit contenir au moins 2 caracteres.`
+        return res.status(400).json({message})
+      }
       return Pokemon.findAndCountAll({
         where: {
           name: {
@@ -12,7 +17,7 @@ module.exports = (app) => {
           }
         },
         order : ['name'],//['name', 'ASC']
-        limit :   5
+        limit :  limit
       }) 
       .then(({count, rows}) => {
         const message = `Il y a ${count} pokemons qui correspondent au terme recherché ${name}.`
